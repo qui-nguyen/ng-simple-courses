5c1668187bfab5ddb09995115b0b0de0e08e3ff8
Qui Nguyen <nguyenhtqui@gmail.com>
2023-11-29 15:15:10 +0100

    feat: show all recettes

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Recipe } from '../type';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.scss']
+  styleUrls: ['./recipe.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
-export class RecipeComponent {
+export class RecipeComponent implements OnInit {
 
+  recipes: Recipe[];
+  selectedRecipes: Recipe[] = [];
+
+  constructor(
+    private recipeService: RecipeService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) { }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe((res: Recipe[]) => this.recipes = res);
+
+  }
+
+  getDisplayedIngredients(recipe: Recipe): string[] {
+    return recipe.ingredients.length > 2
+      ? recipe.ingredients.slice(0, 2).map((ingredient) => ingredient.productBrut.alim_nom_fr)
+      : recipe.ingredients.map((ingredient) => ingredient.productBrut.alim_nom_fr);
+  }
+
+  selectedRecipe(recipe: Recipe) { }
+
+  editRecipe(recipe: Recipe) { }
+
+  detail(recipe: Recipe) { }
+
+  createRecipe() { }
+
+  confirmDelete() { }
 }
