d70bee798437fe78198c97d49e086e548558a672
Qui Nguyen <nguyenhtqui@gmail.com>
2023-11-30 16:50:26 +0100

    feat: add check recipe name when update product

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -61,77 +61,113 @@ export class RecipeComponent implements OnInit {
   //   this.recipeSaved = true;
   //   this.recipeDialog = false;
   // }
+  private showAndResetMessage(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      severity: severity,
+      summary: summary,
+      detail: detail,
+      life: 3000,
+    });
+  }
 
   getNewRecipeData(data: RecipeBody) {
-    if (this.recipe && this.editMode) {
-      this.recipeService.updateRecipe(this.recipe._id, data).subscribe(
-        {
-          next: (result) => {
-            const indexFounded = this.recipes.findIndex((el: Recipe) => el._id === result._id);
-            this.recipes[indexFounded] = result;
-            this.recipeSaved = true;
-          },
-          error: (err) => { console.log(err); },
-          complete: () => { }
-        }
-      )
-    } else {
-      let recipeNameExist = false;
-      this.recipeService.checkRecipeName(data.name).subscribe(
-        {
-          next: (result) => {
-            recipeNameExist = result;
-          },
-          error: (err) => { console.log(err); },
-          complete: () => {
-            let severity = 'success';
-            let summary = 'Félicitation !'
-            let detail = 'Nouvelle recette ajoutée';
+    let severity = 'success';
+    let summary = 'Félicitation !';
+    let detail = 'Nouvelle recette ajoutée';
 
-            if (recipeNameExist) {
-              severity = 'error';
-              summary = 'Erreur !'
-              detail = `${data.name} existe`;
+    const handleRecipeUpdate = (result: Recipe | undefined) => {
+      if (result) {
+        const indexFounded = this.recipes.findIndex((el: Recipe) => el._id === result._id);
+        this.recipes[indexFounded] = result;
+        this.recipeSaved = true;
+        this.recipe = null;
+        this.recipeDialog = false;
 
+        severity = 'success';
+        summary = 'Félicitation !';
+        detail = `${result.name} est modifiée !`;
+      } else {
+        severity = 'error';
+        summary = 'Erreur !';
+        detail = `Une erreur survenue lors de la maj de la recette`;
+      }
+      this.showAndResetMessage(severity, summary, detail);
+    };
+
+    const handleError = (err: any) => {
+      console.log(err);
+      severity = 'error';
+      summary = 'Erreur !';
+      detail = `Une erreur survenue lors de la maj de la recette`;
+      this.showAndResetMessage(severity, summary, detail);
+    };
+
+    if (this.recipe && this.editMode) { // UPDATE
+      // Check if recipe name has changed before making the call
+      if (data.name !== this.recipe.name) {
+        this.recipeService.checkRecipeName(data.name).subscribe({
+          next: (result: boolean) => {
+            if (!result) {
+              this.recipeService.updateRecipe(this.recipe!._id, data).subscribe({
+                next: handleRecipeUpdate,
+                error: handleError,
+              });
             } else {
-              this.recipeService.createRecipe(data).subscribe(
-                {
-                  next: (result) => {
-                    if (result) {
-                      this.recipes.push(result);
-                      this.recipeSaved = true;
-                      this.recipeDialog = false;
-                    }
-                  },
-                  error: (err) => { console.log(err); },
-                  complete: () => { }
-                }
-              );
+              severity = 'error';
+              summary = 'Erreur !';
+              detail = `${data.name} existe`;
+              this.showAndResetMessage(severity, summary, detail);
             }
-
-            this.messageService.add({
-              severity: severity,
-              summary: summary,
-              detail: detail,
-              life: 3000
+          },
+          error: handleError
+        });
+      } else {
+        // If the name hasn't changed, directly update the recipe
+        this.recipeService.updateRecipe(this.recipe!._id, data).subscribe({
+          next: handleRecipeUpdate,
+          error: handleError,
+        });
+      }
+    } else {  // CREATE
+      this.recipeService.checkRecipeName(data.name).subscribe({
+        next: (result: boolean) => {
+          if (!result) {
+            this.recipeService.createRecipe(data).subscribe({
+              next: (result: Recipe | undefined) => {
+                if (result) {
+                  this.recipes.push(result);
+                  this.recipeSaved = true;
+                  this.recipeDialog = false;
+                  this.recipe = null;
+                }
+              },
+              error: (err) => console.log(err),
+              complete: () => this.showAndResetMessage(severity, summary, detail)
             });
+          } else {
+            severity = 'error';
+            summary = 'Erreur !';
+            detail = `${data.name} existe`;
+            this.showAndResetMessage(severity, summary, detail);
           }
-        }
-      );
+        },
+        error: handleError,
+      });
     }
-
   }
+
 
   /*** View detail click ***/
   detail(recipe: Recipe) {
-  
-   }
+
+  }
 
 
   /*** Create recipe ***/
   createRecipe() {
     this.recipe = new Recipe();
     this.recipeDialog = true;
+    this.editMode = false;
   }
 
   confirmDelete() { }
