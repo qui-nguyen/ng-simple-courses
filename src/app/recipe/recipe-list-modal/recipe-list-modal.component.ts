0c916e08d3f27c07703ce85eeb72d35759602048
Qui Nguyen <nguyenhtqui@gmail.com>
2023-12-05 14:41:14 +0100

    refactor: refactor and comment code of recipe-list-modal component

--- src/app/recipe/recipe-list-modal/recipe-list-modal.component.ts
+++ src/app/recipe/recipe-list-modal/recipe-list-modal.component.ts
@@ -1,12 +1,11 @@
+// Import necessary Angular and third-party modules, services, and types
 import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
-
-
 import { RecipeListService } from 'src/app/services/recipe-list.service';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Recipe, RecipeList, ShopListData, ShoppingList } from 'src/app/type';
 
-
+// Define a type that extends Recipe with an additional 'quantity' property
 type RecipeExtendedQty = Recipe & {
   quantity: number;
 };
@@ -16,21 +15,24 @@ type RecipeExtendedQty = Recipe & {
   templateUrl: './recipe-list-modal.component.html',
   providers: [ConfirmationService, MessageService]
 })
-export class RecipeListModalComponent {
+export class RecipeListModalComponent implements OnInit {
+  /*** Input properties to receive data from recipe component ***/
   @Input() selectedRecipes: Recipe[] | null;
-  @Input() recipeListDialog: boolean = false;
+  @Input() recipeListDialog = false;
+
+  /*** Output property to emit events to recipe component ***/
   @Output() recipeListEvent = new EventEmitter<any>();
 
+  /**** Component properties ***/
   recipeList: RecipeList | undefined;
   recipeListName: string | null;
 
   newSelectedRecipes: RecipeExtendedQty[];
-  shopListDialog: boolean = false;
+  shopListDialog = false;
   shopListData: ShopListData | undefined = undefined;
 
   newShopList: ShoppingList | undefined;
-  isSaveShopListClick: boolean;
-
+  isSaveShopListClick = false;
 
   constructor(
     private shopListService: ShoppingListService,
@@ -39,25 +41,26 @@ export class RecipeListModalComponent {
     private messageService: MessageService,
   ) { }
 
-
-  /**** Watch the changement of parent => get values in real time ****/
+  /*** Lifecycle hook => watch for changes in input properties (selectedRecipes) ***/
   ngOnChanges(changes: SimpleChanges): void {
-    if ('selectedRecipes' in changes) {
-      if (this.selectedRecipes) {
-        this.newSelectedRecipes = this.selectedRecipes.map((el: Recipe) => ({ ...el, quantity: 1 }));
-      }
+    // Check for changes in 'selectedRecipes' and update 'newSelectedRecipes' accordingly
+    if ('selectedRecipes' in changes && this.selectedRecipes) {
+      // Map selected recipes to a new array with an additional 'quantity' property
+      this.newSelectedRecipes = this.selectedRecipes.map((el: Recipe) => ({ ...el, quantity: 1 }));
     }
   }
 
+  /*** Lifecycle hook => called after component initialization ***/
   ngOnInit(): void {
+    // Initialize component properties
     this.recipeList = undefined;
     this.newShopList = undefined;
     this.isSaveShopListClick = false;
   }
 
-
-  /**** Recipes List ****/
-  handleOpenConfimDialog() {
+  /*** Handle opening the confirmation dialog => save process ***/
+  handleOpenConfimDialog(): void {
+    // Reset recipe list name and open the confirmation dialog
     this.recipeListName = null;
     this.confirmationService.confirm({
       message: '',
@@ -65,19 +68,22 @@ export class RecipeListModalComponent {
     });
   }
 
-  isRecipeListNameValid() {
-    return !!this.recipeListName; // name not null
+  /*** Check if the recipe list name is valid ***/
+  isRecipeListNameValid(): boolean {
+    return !!this.recipeListName; // Check if name is not null
   }
 
-  handleSaveRecipeList() {
+  /*** Handle saving the recipe list (or both recipe list and shop list) ***/
+  handleSaveRecipeList(): void {
     let isError = false;
     let summary = 'Liste des recettes est sauvegardée !';
-    console.log('Vo');
+
     if (this.recipeListName) {
+      // Check if the recipe list name already exists
       this.recipeListService.checkRecipeListName(this.recipeListName).subscribe({
         next: (isExist) => {
-          // Check if name exist
           if (isExist === false) {
+            // Recipe list name does not exist, create a new recipe list
             this.recipeListService.createRecipeList({
               name: this.recipeListName!,
               recipeListId: this.newSelectedRecipes.map(el => ({ recipeId: el._id, quantity: el.quantity })),
@@ -91,10 +97,11 @@ export class RecipeListModalComponent {
                     this.confirmationService.close();
                   } else {
                     isError = true;
-                    summary = "Erreur survenue !"
+                    summary = "Erreur survenue !";
                   }
                 },
                 complete: () => {
+                  // Create a new shop list based on this recipe list created
                   if (this.isSaveShopListClick) {
                     this.saveShopList();
                   }
@@ -103,38 +110,38 @@ export class RecipeListModalComponent {
             );
 
           } else if (isExist === true) {
+            // Recipe list name already exists
             isError = true;
-            summary = "Nom existe !"
-          } else { // undefind or error when check name
+            summary = "Nom existe !";
+          } else {
+            // Undefined or error when checking the name
             isError = true;
-            summary = "Erreur !"
+            summary = "Erreur !";
           }
         },
         complete: () => {
-          this.messageService.add({
-            severity: `${isError ? 'error' : 'success'}`,
-            summary: summary,
-            detail: '',
-            life: 3000
-          });
+          // Display a message based on the result of the recipe list name check
+          this.showMessage(isError, summary);
         },
-      })
+      });
     }
   }
 
-  handleCloseConfimDialog() {
+  /***  Handle closing the confirmation dialog => Close btn click ***/
+  handleCloseConfimDialog(): void {
     this.confirmationService.close();
   }
 
-  hideDialog() {
+  /***  Hide the dialog and emit an event to the recipe component ***/
+  hideDialog(): void {
     this.shopListDialog = false;
     this.selectedRecipes = null;
     this.recipeList = undefined;
     this.recipeListEvent.emit();
   }
 
-  /**** Shop List ****/
-  saveShopList() {
+  /***  Save the shop list ***/
+  saveShopList(): void {
     if (this.recipeList && this.shopListData) {
       let isError = false;
       let summary = 'Nouvelle liste des courses sauvegardée !';
@@ -157,36 +164,42 @@ export class RecipeListModalComponent {
         },
         complete: () => {
           this.isSaveShopListClick = false;
-          this.messageService.add({
-            severity: `${isError ? 'error' : 'success'}`,
-            summary: summary,
-            detail: '',
-            life: 3000
-          });
+          // Display a message based on the result of shop list creation
+          this.showMessage(isError, summary);
         },
-
-      })
+      });
     }
   }
 
-  showShopList() {
+  /*** Show the shop list => review shop list (not saved) ***/
+  showShopList(): void {
     this.shopListDialog = true;
     this.shopListService.getShopList(this.newSelectedRecipes).subscribe(res => this.shopListData = res);
   }
 
-  returnRecipeList() {
+  /*** Return to the recipe list view from the shop list view ***/
+  returnRecipeList(): void {
     this.shopListDialog = false;
   }
 
-  saveAll() {
-    if (this.recipeList) {
+  /*** Save either the recipe list or both the recipe list and shop list ***/
+  saveAll(): void {
+    if (this.recipeList) { // Case recipeList is saved
       this.saveShopList();
-    } else {
+    } else { // Case recipeList not saved yet
       this.isSaveShopListClick = true;
       this.handleOpenConfimDialog();
       this.handleSaveRecipeList();
     }
-    // this.hideDialog();
-    // this.recipeListEvent.emit();
+  }
+
+  /*** Display a message using the MessageService  ***/
+  private showMessage(isError: boolean, summary: string): void {
+    this.messageService.add({
+      severity: `${isError ? 'error' : 'success'}`,
+      summary: summary,
+      detail: '',
+      life: 3000
+    });
   }
 }
