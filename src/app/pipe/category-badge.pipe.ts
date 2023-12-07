f89fc747de83972b719a2fe838267b0b49334adc
Qui Nguyen <nguyenhtqui@gmail.com>
2023-12-07 14:45:03 +0100

    feat: add color to new categories

--- src/app/pipe/category-badge.pipe.ts
+++ src/app/pipe/category-badge.pipe.ts
@@ -24,6 +24,42 @@ export class CategoryBadgePipe implements PipeTransform {
       case 'Conserve':
         color = 'yellow-600';
         break;
+        case 'Viandes et Poissons':
+        color = 'green-500';
+        break;
+      case 'Sauces':
+        color = 'red-300';
+        break;
+      case "Produits d'hygiène":
+        color = 'orange-300';
+        break;
+      case 'Produits nettoyant':
+        color = 'cyan-200';
+        break;
+      case 'Produits laitiers':
+        color = 'yellow-400';
+        break;
+        case 'Surgelés':
+        color = 'green-900';
+        break;
+      case 'Boissons':
+        color = 'red-700';
+        break;
+      case 'Animalerie':
+        color = 'orange-800';
+        break;
+      case 'Parapharmacie':
+        color = 'cyan-700';
+        break;
+      case 'Bébé':
+        color = 'purple-300';
+        break;
+        case 'Auto, moto':
+        color = 'cyan-300';
+        break;
+      case 'Bricolage':
+        color = 'yellow-200';
+        break;
       default:
         color = 'gray-700';
     }
